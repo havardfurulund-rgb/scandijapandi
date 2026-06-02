7ead4b9db198a85b4210e64bbba2fafd79fff03b
havardfurulund-rgb <havard.furulund@gmail.com>
2026-06-02 11:41:01 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
-import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
   integrations: [
     react(),
     sanity({
