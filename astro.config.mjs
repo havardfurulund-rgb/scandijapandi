ae7e4504d64a119a7d1396caa3211b2be3eaaa06
havardfurulund-rgb <havard.furulund@gmail.com>
2026-06-02 11:03:28 +0200

    Add React integration to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 import { defineConfig } from 'astro/config';
 import sanity from '@sanity/astro';
+import react from '@astrojs/react';
 
 export default defineConfig({
   integrations: [
+    react(),
     sanity({
       projectId: process.env.PUBLIC_SANITY_PROJECT_ID,
       dataset: 'production',
-      studioBasePath: '/studio',
-      useCdn: false,
+      studios: [
+        {
+          name: 'default',
+          title: 'Studio',
+          basePath: '/studio',
+          config: './sanity.config.ts',
+        },
+      ],
     }),
   ],
 });
