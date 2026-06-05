f352c676cdf6840b4ec593c6c47e3919064a31b9
havardfurulund-rgb <havard.furulund@gmail.com>
2026-06-05 09:13:29 +0200

    Update index.ts

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,3 +1,7 @@
-import product from './product';
+import product from './product'
+import curator from './curator' // 1. Legg til denne linjen på toppen
 
-export const schemaTypes = [product];
+export const schemaTypes = [
+  product, 
+  curator // 2. Legg til denne i listen
+]
