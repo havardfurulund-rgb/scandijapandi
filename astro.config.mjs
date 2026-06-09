9a8c9646add5959fb723ee43ca8393a8f36a240d
havardfurulund-rgb <havard.furulund@gmail.com>
2026-06-09 20:48:08 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 import sanity from '@sanity/astro';
 
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
   adapter: netlify(),
   integrations: [
     tailwind({ applyBaseStyles: true }),
+    react(),                    // <--- Viktig for Studio
     sanity({
       projectId: 'v7f0k69w',
       dataset: 'production',
